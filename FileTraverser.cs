cbe8da00a67ce068392f7b2788474592c9099d00
vcicmanec <vladimir.cicmanec@gmail.com>
2015-08-31 20:04:55 +0200

    Initial commit
    Initial commit (with VS solution files and all the other bebop)

---
+++ FileTraverser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lemmator
+{
+    class FileTraverser
+    {
+        public static string[] getFileList()
+        {
+            string[] result = processDirectory(Directory.GetCurrentDirectory() + "\\lemma-source");
+
+            Console.WriteLine("Found {0} .txt files in the current path", result.Length);
+
+            return result;
+        }
+
+        private static string[] processDirectory(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path, "*.txt");
+
+            for(int i = 0; i < dirs.Length; i++)
+            {
+
+                files = files.Concat(processDirectory(dirs[i])).ToArray();
+            }
+
+
+            return files;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using LemmaSharp;
+
+namespace Lemmator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILemmatizer lemmatizer = new LemmatizerPrebuiltCompact(getLanguage(args[0]));
+
+            if(args.Length == 0 || args.Length == 1)
+            {
+                Console.WriteLine("Batch-processing all files contained in the subfolder 'lemma-source' into 'lemma-output'");
+
+                string[] fileList = FileTraverser.getFileList();
+
+                foreach (string file in fileList)
+                {
+                    processFile(file, lemmatizer);
+                }
+            }
+        }
+
+        private static void processFile(string path, ILemmatizer lemmatizer)
+        {
+            Console.WriteLine("Processing file {0}", new FileInfo(path).Name);
+
+            string[] wordList = prepareFileContent(readFile(path));
+
+            List<string> resultList = new List<string>();
+
+            foreach(string word in wordList)
+            {
+                resultList.Add(processWord(word, lemmatizer));
+            }
+
+            writeOutputFile(resultList.ToArray(), path);
+        }
+
+        private static string processWord(string word, ILemmatizer lemmatizer)
+        {
+            return lemmatizer.Lemmatize(word.ToLower());
+        }
+
+        private static string readFile(string path)
+        {
+           try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string text = reader.ReadToEnd();
+                    return text;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File {0} could not be read.", path);
+                return "";
+            }
+        }
+
+        private static string[] prepareFileContent(string content)
+        {
+            string[] result = content.Split(
+                new char[] {' ', ' ', ',', '.', ')', '(', '!', '?', ';', '-', '–','"'},
+                StringSplitOptions.RemoveEmptyEntries
+                );
+
+            return result;
+        }
+
+        private static void writeOutputFile(string[] lemmatizedText, string sourcePath)
+        {
+            string resultPath = sourcePath.Replace("lemma-source", "lemma-output");
+
+            try
+            {
+                new FileInfo(resultPath).Directory.Create();
+                System.IO.File.WriteAllLines(resultPath, lemmatizedText);
+
+                Console.WriteLine("Written output file: {0}", resultPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        private static LanguagePrebuilt getLanguage(string languageParam)
+        {
+            LanguagePrebuilt language = LemmaSharp.LanguagePrebuilt.Slovak;
+
+            string languageNameLog = "Slovak";
+
+            switch (languageParam)
+            {
+                case "cs":
+                    language = LemmaSharp.LanguagePrebuilt.Czech;
+                    languageNameLog = "Czech";
+                    break;
+            }
+
+            Console.WriteLine("Lemmatisation language set to: {0}", languageNameLog);
+
+            return language;
+        }
+    }
+}
